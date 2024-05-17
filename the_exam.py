8161580307227e9c8c47dafad42ee5410bc12eff
grant <gmoore@bishopwalsh.org>
2024-05-17 07:46:41 -0400

    sweet, sweet recursion

--- the_exam.py
+++ the_exam.py
@@ -11,7 +11,9 @@ more space!
 
 import random 
 
+##############################################
 # CONSTANTS
+##############################################
 
 operations = ["-", "+"]
 variables = [ "x", "y", "z"]
@@ -21,9 +23,12 @@ bases = [ "e", "10", "2", "4", "7" ]
 exponents = [ "2", "3", "4", "5", "6", "7" ]
 fractions = [ "(1/2)", "(1/3)", "(1/4)", "(1/5)" ]
 
-# FUNCTIONS
 
-## 1: algebraic symbol randomization functions
+##############################################
+# FUNCTIONS
+##############################################
+## 1: symbol randomization functions
+##############################################
 
 def operation():
     ''' returns a random operation string from the choices "+" or "-".
@@ -36,10 +41,12 @@ def coefficient():
     '''
     return random.choice(coefficients)
 
+
 def solution(): 
     ''' returns a random numeric string between "1" and "10"
     '''
     return random.choice(solutions)
+
 
 def variable():
     ''' returns a random variable string from the choices "x", "y", "z".
@@ -54,7 +61,7 @@ def base():
 
 
 def exponent():
-    ''' return a random exponent string from the choices "2", "3", "4", "5"
+    ''' return a random exponent string from the choices "2", "3", "4", "5", "6", "7"
     '''
     return random.choice(exponents)
 
@@ -64,8 +71,9 @@ def fraction():
     '''
     return random.choice(fractions)
 
-
+##############################################
 ## 2: algebraic expression functions
+##############################################
 
 def identity(var = None):
     ''' returns the argument that is passed in. 
@@ -73,148 +81,230 @@ def identity(var = None):
     '''
     return variable() if var is None else var
 
-def scalar_expression(var = None, function = identity):
+##############################################
+### a: one-place expressions
+##############################################
+
+def scalar_expression(var = None, function = identity, a = None):
     ''' returns a scalar expression: a * f(x)
     '''
     func = function(var)
     
-    a = coefficient()
+    a = coefficient() if a is None else a
 
     return f"{a}*{func}"
 
-def product_expression(var = None, function_1 = identity, function_2 = identity):
-    ''' returns a product of two expressions
-    '''
-    
-    func_1 = function_1(var)
-    func_2 = function_2(var)
-
-    return f"({func_1})*({func_2})"
-
-def quotient_expression(var = None, function_1 = identity, function_2 = identity):
-    ''' returns the quotient of two expressions
-    '''
-
-    func_1 = function_1(var)
-    func_2 = function_2(var)
-
-    return f"(({func_1})/({func_2}))"
-
-def square_root_expression(var = None, function = identity):
+def root_expression(var = None, function = identity, n = None, parenthesis = False):
     ''' returns a square root expression string
     '''
 
     func = function(var)
 
-    return f"sqrt({func})"
+    n = exponent() if n is None else n
+
+    res = f"{n}root({func})"
+
+    if parenthesis:
+        return f"({res})"
+    return res
 
 
-def power_expression(var = None, function = identity):
+def power_expression(var = None, function = identity, exp = None, parenthesis = False):
     ''' returns an exponential expression string
     '''
     
     func = function(var)
 
-    exp = exponent()
+    exp = exponent() if exp is None else exp
 
-    return f"({func}^{exp})"
+    res = f"{func}^{exp}"
+
+    if parenthesis:
+        return f"({res})"
+    return res
 
 
-def log_expression(var = None, function = identity):
+def log_expression(var = None, function = identity, b = None, parenthesis = False):
     ''' returns a log expression string
     '''
 
     func = function(var)
     
-    b = base()
+    b = base() if b is None else b
     
-    return f"log_{b}({func})"
+    res = f"log_{b}({func})"
 
-def linear_expression(var = None, function = identity):
+    if parenthesis:
+        return f"({res})"
+    return res
+
+##############################################
+### b: two place expressions
+##############################################
+
+def operation_expression(var = None, function_1 = identity, function_2 = identity, op = None, parenthesis = False):
+    ''' returns a sum or difference of two expressions
+    ''' 
+
+    func_1 = function_1(var)
+    func_2 = function_2(var)
+
+    op = operation() if op is None else op
+
+    res = f"{func_1} {op} {func_2}"
+
+    if parenthesis:
+        return f"({res})"
+    return res
+
+##############################################
+### c: composite expressions
+##############################################
+
+def linear_expression(var = None, function = identity, parenthesis = False):
     ''' returns a linear expression string, a * f(x) + b
     '''
 
-    func = function(var)
+    res = operation_expression(
+        var, 
+        lambda x: scalar_expression(
+            var = x,
+            function = function
+        ), 
+        lambda x: coefficient()
+    )
 
-    a = coefficient()
-    b = coefficient()
-    op = operation()
-
-    return f"{a} * {func} {op} {b}"
+    if parenthesis:
+        return f"({res})"
+    return res
 
 
-def quadratic_expression(var = None, function = identity):
+def quadratic_expression(var = None, function = identity, parenthesis = False):
     ''' returns a quadratic expression string: a * f(x) ^ 2 + b * f(x) + c
     '''
 
-    func = function(var)
+    res =  operation_expression(
+        var,
+        lambda x: power_expression(
+            var = x, 
+            exp = 2
+        ),
+        linear_expression
+    )
+    
+    if parenthesis:
+        return f"({res})"
+    return res
 
-    quad_term = f"{func}^2"
-    linear_term = linear_expression(func)
-    op = operation()
-
-    return f"{quad_term} {op} {linear_term}"
 
 ## 3: algebraic equation function
    
-def equation(expression = identity):
+def equation(expression = identity, sol = None):
     ''' pass in an algebraic expression (function) and set it equal to a number!
     '''
 
-    return expression() + " = " + solution()
-
+    if sol is None:
+        return expression() + " = " + solution()
+    return expression() + " = " + sol
+    
 # THE EXAM
 
 if __name__ == "__main__":
     # The script entrypoint. This is where everything starts!
 
     print("THE FINAL EXAM \n\n")
-    print("Part 1: Simplify the following expressions. \n \n")
-
+    
     ## PART 1: SIMPLICATION
-    q1 = product_expression(
+    ### Question 1
+    q1 = operation_expression(
         var = identity(), 
-        function_1 = power_expression, 
-        function_2 = power_expression
+        op = "*",
+        function_1 = lambda x: power_expression(
+            x, 
+            parenthesis = True
+        ), 
+        function_2 = lambda x: power_expression(
+            x, 
+            parenthesis = True
+        ),
     )
-    q2 = product_expression(
+    ### Question 2
+    q2 = operation_expression(
         var = identity(), 
-        function_1 = lambda y: power_expression(y, function = scalar_expression), 
-        function_2 = linear_expression
+        op = "*",
+        function_1 = lambda y: power_expression(
+            y, 
+            function = scalar_expression,
+            parenthesis = True
+        ), 
+        function_2 = lambda y: linear_expression(
+            y,
+            parenthesis = True
+        )
     )
-    q3  = quotient_expression(
+    ### Question 3
+    q3  = operation_expression(
         var = identity(),
-        function_1 = power_expression,
-        function_2 = power_expression
+        op = "/",
+        function_1 = lambda y: power_expression(
+            y,
+            parenthesis = True
+        ),
+        function_2 = lambda y: power_expression(
+            y, 
+            parenthesis = True
+        )
     )
+    ### Question 4
     q4 = power_expression(
         var = identity(),
-        function = power_expression
+        function = lambda y: power_expression(
+            y, 
+            parenthesis = True
+        )
     )
     
-    print("\t 1. " + q1 + " \n")
-    print("\t 2. " + q2 + " \n")
-    print("\t 3. " + q3 + " \n")
-    print("\t 4. " + q4 + " \n")
-
-    print("\t TODO: do this part! \n \n") 
-    
-    print("Part 2: Solve the following equations using any method you choose. \n \n")
-
-    ## PART 2: SOLUTION
-
-    # NOTE: a function is being passed into a function here, i.e. f(g(x)) or *composition*.
-    #       Notice the inner function isn't suffixed with "()", so it is not called.
-    #       Instead, the "name" of the function is being passed into the "equation" function.
-    q1 = equation(quadratic_expression)
-    # NOTE: using an anoynmous "lambda function" to override default argument in the inner
-    #       function. This is like a triple composition! f(g(h(x)))! Oh my!
-    q2 = equation(lambda: square_root_expression(function = linear_expression))
-    q3 = equation(lambda: log_expression(function = power_expression))
-    q4 = equation(lambda: log_expression(function=quadratic_expression))
-
-    print("\t 1. "  + q1 + "\n" )
+    print("Part 1: Choose 3 of the 6 following problems to complete. Simplify each expression you choose using any method. \n \n")
+    print("\t 1. " + q1 + "\n")
     print("\t 2. " + q2 + "\n")
     print("\t 3. " + q3 + "\n")
     print("\t 4. " + q4 + "\n")
+    print("\t TODO: two more! \n\n") 
+    
+    print("Part 2: Choose 3 of the following 6 problems to complete. Graph each function on the x-y plane as accurately as possible. \n \n")
+    print("\t TODO \n \n")
+
+    ## PART 3: SOLUTION
+    q1 = equation(quadratic_expression)
+    q2 = equation(
+        lambda: root_expression(
+            function = linear_expression, 
+            n = "2"
+        )
+    )
+    q3 = equation(
+        lambda: log_expression(
+            function = power_expression
+        )
+    )
+    q4 = equation(
+        lambda: log_expression(
+            function = quadratic_expression
+        )
+    )
+    q5 = equation(
+        lambda: linear_expression(
+            function = power_expression
+        ),
+        coefficient()
+    )
+
+    print("Part 3: Choose 3 of the 6 following problems to complete. Solve each equation you choose using any method. \n \n")
+    print("\t 1. " + q1 + "\n" )
+    print("\t 2. " + q2 + "\n")
+    print("\t 3. " + q3 + "\n")
+    print("\t 4. " + q4 + "\n")
+    print("\t 5. " + q5 + "\n")
+    print("\t TODO: one more! \n\n") 
+
 
